3c3451c53115c051159649dd7c088052bca76348
Denis Esie <nesseratious@gmail.com>
2024-08-09 23:11:32 +0300

    Update versions

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.10
 
 import PackageDescription
 
 let package = Package(
     name: "Loaf",
     platforms: [
-        .iOS(.v9)
+        .iOS(.v16), .macCatalyst(.v16)
     ],
     products: [
         .library(
